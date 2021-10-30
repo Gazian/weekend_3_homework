4648f5785c6b2d100f2f3320c5b9715db4f23622
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-30 22:52:48 +0100

    removing game_tests

--- models/rps_game.py
+++ models/rps_game.py
@@ -1,27 +1,19 @@
-class Game:
-
-    def __init__(self,player_1,player_1_choice,player_2,player_2_choice):
-        self.player_1 = player_1
-        self.player_1_choice = player_1_choice
-        self.player_2 = player_2
-        self.player_2_choice = player_2_choice
-
-    def determine_result(self,player_1_choice,player_2_choice):
-        if player_1_choice == player_2_choice:
-            result = "The game is a Draw"
-        elif player_1_choice == "rock" and player_2_choice == "scissors":
-            result = f"Player 1 wins with {player_1_choice}"
-        elif player_1_choice == "scissors" and player_2_choice == "rock":
-            result = f"Player 2 wins with {player_2_choice}"
-        elif player_1_choice == "paper" and player_2_choice == "rock":
-            result = f"Player 1 wins with {player_1_choice}"
-        elif player_1_choice == "rock" and player_2_choice == "paper":
-            result = f"Player 2 wins with {player_2_choice}"
-        elif player_1_choice == "scissors" and player_2_choice == "paper":
-            result = f"Player 1 wins with {player_1_choice}"
-        elif player_1_choice == "paper" and player_2_choice == "scissors":
-            result = f"Player 2 wins with {player_2_choice}"
-        return result
+def determine_result(player_1_choice,player_2_choice):
+    if player_1_choice == player_2_choice:
+        result = "The game is a Draw"
+    elif player_1_choice == "rock" and player_2_choice == "scissors":
+        result = "Player 1 wins with rock"
+    elif player_1_choice == "scissors" and player_2_choice == "rock":
+        result = "Player 2 wins with rock"
+    elif player_1_choice == "paper" and player_2_choice == "rock":
+        result = "Player 1 wins with paper"
+    elif player_1_choice == "rock" and player_2_choice == "paper":
+        result = "Player 2 wins with paper"
+    elif player_1_choice == "scissors" and player_2_choice == "paper":
+        result = "Player 1 wins with scissors"
+    elif player_1_choice == "paper" and player_2_choice == "scissors":
+        result = "Player 2 wins with scissors"
+    return result
 
 
     
